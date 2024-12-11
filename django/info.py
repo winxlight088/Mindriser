112963956441160a2875778363b8cdc41b8f0e0d
Anisha Rai <anisha6rai@gmail.com>
2024-12-11 21:54:47 +0545

    changes in master branch

---
+++ django/info.py
@@ -0,0 +1,5 @@
+#Django - framework, used for FUll stack development
+#uses MVT architecture
+#Model- database
+#View - where we write Logics, it help interact with models and templates
+#templates- design, statics files, HTML, CSS, imgs etc
